5aa790c297613728dbae36a816504660da0e3d3f
anhhanuman <maivuonganh230489@gmail.com>
2021-12-19 15:47:22 +0700

    search element by

--- cypress/integration/first-test.spec.js
+++ cypress/integration/first-test.spec.js
@@ -1,1 +1,13 @@
 /// <reference types="cypress" />
+describe('the first test suite', () => {
+    it('the first test case', () => {
+        //search element by tag name
+        cy.get('input')
+        cy.get('#inputEmail')
+        cy.get('.input-full-width')
+        // by attribute name
+        cy.get('[placeholder]')
+        //by attribute name and value
+
+    })
+})
