18d2cb9aee73055dc33fc6296209b8bfa946aafd
anhhanuman <maivuonganh230489@gmail.com>
2021-12-20 14:09:36 +0700

    demonstrate error when using find() without parents()

--- cypress/integration/first-test.spec.js
+++ cypress/integration/first-test.spec.js
@@ -53,5 +53,7 @@ describe('the first test suite', () => {
             .should('contain','Sign in')
             .parents('form')
             .find('nb-checkbox').click()
+        //error if using only find() without parents() or parent()
+        cy.find('button')
     })
 })
