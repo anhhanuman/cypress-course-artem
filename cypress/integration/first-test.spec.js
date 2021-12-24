00595cb02ac8c349452749ff4882e4f87f90769a
anhhanuman <maivuonganh230489@gmail.com>
2021-12-24 09:34:01 +0700

    check method only works with input element type, and type should be only radio or checkbox

--- cypress/integration/first-test.spec.js
+++ cypress/integration/first-test.spec.js
@@ -162,7 +162,7 @@ describe('the first test suite', () => {
         cy.contains('Modal & Overlays').click()
         cy.contains('Toastr').click()
         cy.get('[type="checkbox"]').check({force:true})
-        //check method only check to the UNCHECK
+        //check method only check to the UNCHECK boxes
         //IF you want to  uncheck you have to use the click()
         cy.get('[type="checkbox"]').eq(1).check({force:true})
         cy.get('[type="checkbox"]').eq(0).check({force:true})//not work
