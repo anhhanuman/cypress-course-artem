c33562f8fcacc0e8e3d7ebc8d847ddd6745034b1
anhhanuman <maivuonganh230489@gmail.com>
2021-12-30 06:40:15 +0700

    parameterize the method

--- cypress/support/page_objects/form-layout-page.js
+++ cypress/support/page_objects/form-layout-page.js
@@ -1,8 +1,8 @@
 export class FormLayoutPage {
-    submitInlineFormWithNameAndEmail() {
+    submitInlineFormWithNameAndEmail(name, email) {
         cy.contains('nb-card', 'Inline form').find('form').then(form => {
-            cy.wrap(form).find('input[placeholder="Jane Doe"]').click().type('Anh Mai')
-            cy.wrap(form).find('input[placeholder="Email"]').click().type('anhmai@autotest.vn')
+            cy.wrap(form).find('input[placeholder="Jane Doe"]').click().type(name)
+            cy.wrap(form).find('input[placeholder="Email"]').click().type(email)
             cy.wrap(form).find('[type="checkbox"]').check({force: true})
             cy.wrap(form).submit()
             //only use for form, if DOM element has form tag
