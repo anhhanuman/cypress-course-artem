37b2b693c4485be6d64004136c53646887599624
anhhanuman <maivuonganh230489@gmail.com>
2021-12-30 06:24:40 +0700

    more test on page object

--- cypress/integration/testWithPageObjects.js
+++ cypress/integration/testWithPageObjects.js
@@ -1,16 +1,20 @@
-import {navigationPage} from "../support/page_objects/navigationPage";
+import {navigationPage} from "../support/page_objects/navigation-page";
 
 describe('Test with page object', () => {
     beforeEach('open application', () => {
         cy.visit('/')
     })
 
-    it.only('verify navigations across pages', () => {
+    it('verify navigations across pages', () => {
         navigationPage
             .displayFormLayout()
             .displayDatePickerPage()
             .displaySmartTable()
             .displayToolTip()
             .displayToastr()
+    })
+
+    it.only('submit inline and basic form and select tomorrow from the calendar', () => {
+        navigationPage.displayFormLayout()
     })
 })
