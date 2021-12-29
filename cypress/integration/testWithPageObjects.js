0de3eef1711f207743622d1e8030afc762ceea73
anhhanuman <maivuonganh230489@gmail.com>
2021-12-29 21:51:21 +0700

    test with page object

--- cypress/integration/testWithPageObjects.js
+++ cypress/integration/testWithPageObjects.js
@@ -1,9 +1,16 @@
+import {navigationPage} from "../support/page_objects/navigationPage";
+
 describe('Test with page object', () => {
     beforeEach('open application', () => {
         cy.visit('/')
     })
 
-    it.only('verify navigations across pages',()=>{
-
+    it.only('verify navigations across pages', () => {
+        navigationPage
+            .displayFormLayout()
+            .displayDatePickerPage()
+            .displaySmartTable()
+            .displayToolTip()
+            .displayToastr()
     })
 })
