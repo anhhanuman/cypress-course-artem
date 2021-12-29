4a1cffd95be99a623fa4c7ca1419fec1a85ec2ef
anhhanuman <maivuonganh230489@gmail.com>
2021-12-30 06:34:36 +0700

    correct error

--- cypress/support/page_objects/form-layout-page.js
+++ cypress/support/page_objects/form-layout-page.js
@@ -1,7 +1,7 @@
 export class FormLayoutPage {
     submitInlineFormWithNameAndEmail() {
-        cy.contains('nb-card', 'Inline Form').find('form').then(form => {
-            cy.wrap(form).find('input[placeholder=Jane Doe]').click().type('Anh Mai')
+        cy.contains('nb-card', 'Inline form').find('form').then(form => {
+            cy.wrap(form).find('input[placeholder="Jane Doe"]').click().type('Anh Mai')
             cy.wrap(form).find('input[placeholder="Email"]').click().type('anhmai@autotest.vn')
             cy.wrap(form).find('[type="checkbox"]').check({force: true})
             cy.wrap(form).submit()
